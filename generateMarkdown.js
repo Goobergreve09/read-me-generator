42060360ae32fde13a704b3dd289f74530e1b829
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-03 00:40:52 -0500

    adding js content

---
+++ generateMarkdown.js
@@ -0,0 +1,102 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  const badges = [
+    '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
+    '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+    '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)',
+    '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+    '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+
+]
+
+  if (license === 'MIT') {
+    return badges[0];
+  } else if (license === 'GPL v3') {
+    return badges[1];
+  } else if (license === 'Apache 2.0') {
+    return badges[2];
+  } else if (license === 'ISC') {
+    return badges[3];
+  } else if (license === 'GNU AGPL v3') {
+  return badges[4];
+  } else if (license === 'The Unlicense') {
+    return badges [5];
+  } else if (license === 'WTFPL') {
+    return badges [6];
+  } else if (license === 'Mozilla Public License 2.0') {
+    return badges [7];
+  } else if (license === 'BSD 3-Clause') {
+    return badges [8];
+  }
+  return ''; // If no valid license is provided, return an empty string
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+  const licenseBadge = renderLicenseBadge(answers.license);
+
+  return `# ${answers.title}
+  
+## Description
+
+${answers.description}
+
+${answers.motivation}
+  
+## Table of Contents
+
+1. [Description](#description) 
+2. [How to Install](#installation)
+3. [How to Use](#usage)
+4. [Licensing](#license)
+5. [Testing](#tests)
+6. [Contact Me](#questions)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributions
+
+${answers.credits}
+
+## License
+
+${answers.license}
+
+${licenseBadge}
+
+## Tests
+
+${answers.test}
+
+## Questions
+
+If you have any additional questions you can contact me by using the following:
+
+ Github Profile: [${answers.questions}](https://www.github.com/${answers.questions})
+
+ Email Address: ${answers.email}
+
+
+`}
+
+module.exports = generateMarkdown;//Gives access to this function in any files necessary
+
---
+++ index.js
@@ -0,0 +1,123 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { writeFile } = require('fs').promises;
+
+const license = [
+    'MIT',
+    'GPL v3',
+    'Apache 2.0',
+    'ISC',
+    'GNU AGPL v3',
+    'The Unlicense',
+    'WTFPL',
+    'Mozilla Public License 2.0',
+    'BSD 3-Clause'
+
+]
+
+const fileName  = 'README.md';
+
+const generateMarkdown = require('./generateMarkdown'); //imports an finds the function named generateMarkdown from another file
+const renderLicenseBadge = require('./generateMarkdown');
+
+const enter = "Type 'Enter' to skip or save.";
+const arrowKeys = "Use Arrow Keys to Navigate Licenses."
+const enterarrowKeys = `\x1b[33m${arrowKeys}\x1b[0m`;
+const enterYellow = `\x1b[33m${enter}\x1b[0m`;
+const success = 'README.md created successfully!';
+const successGreen = `\x1b[32m${success}\x1b[0m`;
+
+// TODO: Create an array of questions for user input
+const promptUser = () => {
+    return inquirer.prompt([
+  
+    {
+        type:'input',
+        name: 'questions',
+        message: `What is your github username? ${enterYellow}\n`
+      },
+      {
+        type:'input',
+        name: 'email',
+        message: `What is your E-mail address? ${enterYellow}\n`
+      },
+    {  type: 'input',
+    name: 'title',
+    message: `What is a good title for your project? ${enterYellow}\n`
+  } ,
+  {
+    type:'input',
+    name: 'description',
+    message: `\nGive a thorough description of the project. What? Why? and How? ${enterYellow}\n`
+  },
+  {
+    type:'input',
+    name: 'motivation',
+    message: `\nWhat was your motivation for this project? ${enterYellow}\n`
+  },
+  {
+    type:'input',
+    name: 'tableOfcontents',
+    message: `\nA table of Contents will be created here once prompt is completed. ${enterYellow}\n`
+  },
+  {
+    type:'input',
+    name: 'installation',
+    message: `\nHow might someone install this project? Use step by step detailed instructions. ${enterYellow}\n`
+  },
+  {
+  type:'input',
+  name: 'usage',
+  message: `\nHow does one use or run the project? ${enterYellow}\n`
+},
+{
+    type:'input',
+    name: 'credits',
+    message: `\nDid anyone else contribute to this project? If yes link their github profiles here. ${enterYellow}\n`
+  },
+{
+  type:'list',
+  name: 'license',
+  message: `\nWhat license did you use for this project? ${enterarrowKeys}\n`,
+  choices: license,
+
+
+},
+{
+    type:'input',
+    name: 'test',
+    message: `\nHow do you run the tests created for this project? ${enterYellow}\n`
+},
+]);
+};
+
+
+// inquirer.prompt(questions)
+//   .then((answers) => {
+   
+//     const markdownContent = generateMarkdown(answers);
+
+ 
+
+
+
+// // TODO: Create a function to write README file
+// fs.writeFile(fileName, markdownContent, (err) =>
+//     err ? console.log(err) : console.log(`${successGreen}`)
+// );
+// });
+
+// TODO: Create a function to initialize app
+    const init = () => {
+        promptUser()
+          .then((answers) => writeFile(fileName, generateMarkdown(answers)))
+          .then(() => console.log('Successfully wrote to readME.md'))
+          .catch((err) => console.error(err));
+      };
+      
+
+
+// Function call to initialize app
+init();
+
